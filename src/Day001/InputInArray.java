8f03d04f2cb0fe24e3a64c0e3df9efe55e85d9f7
Laserarjun876 <laserarjun876@gmail.com>
2019-10-03 10:08:34 +0545

    Day001 added

---
+++ src/Day001/InputInArray.java
@@ -0,0 +1,22 @@
+package Day001;
+
+import java.util.Scanner;
+
+//taking input as array uing for loop
+public class InputInArray {
+	public static void main(String[] args) {
+		Scanner obj=new Scanner (System.in);
+		int[] myIntArray=new int[20];
+		System.out.println("Enter any 10 numbers :");
+		for(int i=0;i<10;i++)
+		{
+			myIntArray[i]=obj.nextInt();
+		}
+		System.out.println("Entered numbers are :");
+		for(int i=0;i<10;i++)
+		{
+			System.out.print(myIntArray[i]+" ");
+		}
+	}
+
+}
